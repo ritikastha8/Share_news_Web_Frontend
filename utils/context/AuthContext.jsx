4b46e92d05863f3a2d17b157240b12b7fd8b01a7
Ritika Shrestha <shrestharitika998@gmail.com>
2025-02-25 21:30:52 +0545

    AuthContext.jsx added

---
+++ utils/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useContext, useEffect } from 'react';
+
+const AuthContext = createContext(null);
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(() => {
+    const savedAuth = localStorage.getItem('auth');
+    return savedAuth ? JSON.parse(savedAuth) : null;
+  });
+
+  useEffect(() => {
+    if (auth) {
+      localStorage.setItem('auth', JSON.stringify(auth));
+    } else {
+      localStorage.removeItem('auth');
+    }
+  }, [auth]);
+
+  const login = (userData) => {
+    setAuth(userData);
+  };
+
+  const logout = () => {
+    setAuth(null);
+    localStorage.removeItem('auth');
+  };
+
+  return (
+    <AuthContext.Provider value={{ auth, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
