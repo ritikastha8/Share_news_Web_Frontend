371a54e90d0e2e64f157fde694be9d9c6d4ef904
Ritika Shrestha <shrestharitika998@gmail.com>
2025-02-25 21:20:53 +0545

    Landing.jsx added

---
+++ src/components/screens/landing/Landing.jsx
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import NewsFeed from "./NewsFeed";
+
+const Landing = () => {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <NewsFeed />
+    </div>
+  );
+};
+
+export default Landing;
